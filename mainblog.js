40e8cd3e12c407801445ccf19895650932496bc6
andy20080529 <60836494+andy20080529@users.noreply.github.com>
2020-10-08 14:53:01 +0800

    Update mainblog.js

--- mainblog.js
+++ mainblog.js
@@ -1,23 +1,11 @@
-/*
-function load(name)
-{
-    let xhr = new XMLHttpRequest(),okStatus = document.location.protocol === "file:" ? 0 : 200;
-    xhr.open('GET', name, false);
-    xhr.overrideMimeType("text/html;charset=utf-8");//默认为utf-8
-    xhr.send(null);
-    return xhr.status === okStatus ? xhr.responseText : null;
-}
- 
-let text = load("all.json");
-//console.log(text);
-*/
 var back;
 
 var req = new XMLHttpRequest();
 req.open('GET','all.json',false);
+req.overrideMimeType("text/plain;charset=utf-8");//默认为utf-8
 
 req.onload = function() {
-    if (req.status == 200) {
+    if (req.status == 200&&req.readyState) {
       // do what you want, here, with the req.response
       // take a look at the object that gets returned, you may need
       // to call JSON.parse(), etc.
